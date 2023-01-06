e0cf59cc3d027aebb3c080ed6ae7ce90400aa579
Jonathan Fritsch <jdfritsch76@gmail.com>
2023-01-06 12:43:05 +0100

    Improve RadioGroup
    Add callback for changed value
    Support string-backed enums

--- src/components/RadioGroup.tsx
+++ src/components/RadioGroup.tsx
@@ -1,33 +1,41 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import styles from "../style/radiogroup.module.css"
 
-type RadioItem = {
-    id: string,
+type RadioItem<T extends string> = {
+    id: T,
     label: string,
 }
 
-type RadioGroupProps = {
+type RadioGroupProps<T extends string> = {
     name: string,
-    value: string,
-    items: RadioItem[],
+    value: T,
+    items: RadioItem<T>[],
+    onChange?: (value: T) => void,
 }
 
-export default function RadioGroup(props: RadioGroupProps) {
+export default function RadioGroup<T extends string>(props: RadioGroupProps<T>) {
     const [value, setValue] = useState(props.value);
+    const onChange = props.onChange;
+
+    useEffect(() => {
+        if (onChange) {
+            onChange(value);
+        }
+    }, [value, onChange])
 
     return (
         <fieldset className={styles.set}>
             <div className={styles.container}>
                 {props.items.map((item) =>
                     <div key={item.id}>
-                        <input
-                            type="radio"
-                            value={item.id}
-                            checked={item.id === value}
-                            onChange={() => setValue(item.id)}
-                        />
-                        <label htmlFor={item.id}>
+                        <label>
+                            <input
+                                type="radio"
+                                value={item.id}
+                                checked={item.id === value}
+                                onChange={() => setValue(item.id)}
+                            />
                             {item.label}
                         </label>
                     </div>
